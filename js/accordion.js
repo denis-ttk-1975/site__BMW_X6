0c7de21a4c1330970d661321391e77c9500f7cc5
denis-ttk-1975 <denis.ttk@gmail.com>
2021-05-20 12:32:14 +0300

    accordion action added

--- js/accordion.js
+++ js/accordion.js
@@ -5,13 +5,29 @@ document.addEventListener("DOMContentLoaded", () => {
 	const featureSubElems = document.querySelectorAll('.feature-sub');
 	// console.log (featureSubElems);
 
-	for (let i = 0; i < featureSubElems.length; i++) {
-		featureLinkElems[i].addEventListener('click', () => {
-			console.log(i)
-			// featureSubElems[i].classList.remove('')
-		})
+	// for (let i = 0; i < featureSubElems.length; i++) {
+	// 	featureLinkElems[i].addEventListener('click', () => {
+	// 		// console.log(i)
+	// 		featureSubElems[i].classList.toggle('hidden');
+	// 		featureLinkElems[i].classList.toggle('feature__link_active')
+	// 	})
 		// console.log (featureLinkElems[i])
-	}
+
+		featureLinkElems.forEach((butt, idx) => {
+			butt.addEventListener('click', () => {
+				featureSubElems.forEach((featureSubElem) => {
+					featureSubElem.classList.add('hidden')
+				})
+				featureLinkElems.forEach((featureLinkElem) => {
+					featureLinkElem.classList.remove('feature__link_active')
+				} )
+				
+				
+				featureSubElems[idx].classList.remove('hidden');
+				butt.classList.add('feature__link_active')
+			})
+		})
+	
 	
 
 })
