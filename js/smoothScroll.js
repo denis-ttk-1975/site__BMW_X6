6b68db1744d1053f02298b8e56a444684189ccde
denis-ttk-1975 <denis.ttk@gmail.com>
2021-05-20 16:34:50 +0300

    smoothScroll done

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,7 +1,16 @@
 const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
 
 smoothScrollElems.forEach (link => {
-	link.addEventListener('click', () => {
-		console.log(link)
+	link.addEventListener('click', (event) => {
+		
+		event.preventDefault()
+		// console.log(event)
+
+		const id = link.getAttribute('href').substring(1);
+		// console.log ("id", id)
+
+		document.getElementById(id).scrollIntoView({
+			behavior: 'smooth'
+		})
 	})
 })
