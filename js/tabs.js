8090e39e6121297de4cf70d0b7ec3caf3cbc6a64
denis-ttk-1975 <denis.ttk@gmail.com>
2021-05-20 20:11:15 +0300

    title toggle added

--- js/tabs.js
+++ js/tabs.js
@@ -1,13 +1,17 @@
 const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
 const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
+const titleElems = document.querySelectorAll('.design__title'); 
 
 console.log (tabsHandlerElems)
 console.log (tabsFieldElems)
 
 
 for (const tab of tabsHandlerElems) {
-	console.log(1)
+	
 	tab.addEventListener('click', () => {
+		titleElems.forEach(title => {
+			title.classList.toggle('hidden')
+		})
 		tabsHandlerElems.forEach(item => {
 			if (item == tab) {
 				item.classList.add('design-list__item_active')
