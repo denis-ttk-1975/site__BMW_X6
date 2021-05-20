e4dfc670d4b567f35e5f190e0c6222ab51a3a243
denis-ttk-1975 <denis.ttk@gmail.com>
2021-05-20 11:26:40 +0300

    initial commit

---
+++ js/accordion.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+	
+	const featureLinkElems = document.querySelectorAll('.feature__link');
+	// console.log (featureLinkElems);
+	const featureSubElems = document.querySelectorAll('.feature-sub');
+	// console.log (featureSubElems);
+
+	for (let i = 0; i < featureSubElems.length; i++) {
+		featureLinkElems[i].addEventListener('click', () => {
+			// console.log(i)
+			featureSubElems[i].classList.remove('')
+		})
+		// console.log (featureLinkElems[i])
+	}
+	
+
+})
+
