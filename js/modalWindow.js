fadd022f459249a5ba05d183f561318989a755f4
denis-ttk-1975 <denis.ttk@gmail.com>
2021-05-21 12:48:57 +0300

    modalWindow done

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,12 +1,43 @@
-const moreElem = document.querySelectorAll('.more');
-const modalElem = document.querySelectorAll('.modal');
+
+const modalElem = document.querySelector('.modal');
+const moreElems = document.querySelectorAll('.more');
+
+console.log(moreElems)
+console.log(modalElem)
 
 const openModal = () => {
 	modalElem.classList.remove('hidden');
 }
-;
+
 const closeModal = () => {
+	modalElem.classList.add('hidden');
+}
+	
+moreElems.forEach (item => {
+	item.addEventListener('click', openModal)
+})
 
-};
+modalElem.addEventListener('click', (event) => {
+	const target = event.target
+	if (target.classList.contains('overlay') || 
+	target.classList.contains('modal__close')) {
+		closeModal()
+	}
 
-moreElem.addEventListener('click', openModal())
+	// ПОКРАСИМ В КРАСНЫЙ ЦВЕТ ЗАГОЛОВОК МОДАЛЬНОГО ОКНА
+
+	if (target.classList.contains('modal__title')) {
+		target.style.color = 'red'
+	}
+})
+
+
+
+
+
+// const moreElem = document.querySelectorAll('.more');
+// const modalElem = document.querySelectorAll('.modal');
+
+
+
+// modalElem.classList.remove('hidden')
