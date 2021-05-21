bc556a4eb196c6f5a1c7d8c3291748a500642242
denis-ttk-1975 <denis.ttk@gmail.com>
2021-05-21 16:21:23 +0300

    blockScroll 2 version

--- js/blockScroll.js
+++ js/blockScroll.js
@@ -1,7 +1,9 @@
 const disableScroll = () => {
-	document.body.style.overflow = 'hidden'
+	// document.body.style.overflow = 'hidden';
+	document.body.style.cssText = 'overflow: hidden; position: relative; height: 100vh;';
 }
 
 const enableScroll = () => {
-	document.body.style.overflow = ''
+	// document.body.style.overflow = '';
+	document.body.style.cssText = '';
 }
